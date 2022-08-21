3fdc5ec224dd43b98bf6d529806c53f1d6a4eee6
Shubham <bluequbits@gmail.com>
2022-08-21 20:44:42 +0530

    added cntrl q to quit

--- kilo.c
+++ kilo.c
@@ -4,33 +4,54 @@
 #include <ctype.h>
 #include <stdio.h>
 
+//defines
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
+void die(const char*s){
+	perror(s);
+	exit(1);
+}
+
 void disableRawMode(){
-   tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
+  if (tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios)==-1)
+	die("tcsetattr");
 }
 
 void enableRawMode(){
 
 //original raw termios;
 
-tcgetattr(STDIN_FILENO,&orig_termios);
+ if (tcgetattr(STDIN_FILENO,&orig_termios)==-1) die("tcgetattr");
 atexit(disableRawMode);
  struct termios raw = orig_termios;
  tcgetattr(STDIN_FILENO, &raw);
- raw.c_lflag &= ~(IXON);
- raw.c_lflag &= ~(ECHO|ICANON|ISIG);
+//input flags to turn off to prevent weird behaviour by terminal
+ raw.c_iflag &= ~(IXON|ICRNL|BRKINT|INPCK|ISTRIP);
+//opost to disable the conversion of \n to \r\n
+ raw.c_oflag &= ~(OPOST);
+ raw.c_cflag |= (CS8);
+//disable various ctrl+key options
+ raw.c_lflag &= ~(ECHO|ICANON|ISIG|IEXTEN);
+ //timer for read input
+ raw.c_cc[VMIN] =0;
+ raw.c_cc[VTIME] =1;
  tcsetattr(STDIN_FILENO,TCSAFLUSH, &raw);
 }
 
 int main(){
   enableRawMode();
-  char c;
-  while(read(STDIN_FILENO,&c,1)==1 && c!='q'){
+
+  while(1){
+	char c='\0';
+	read(STDIN_FILENO,&c,1);
 	if(iscntrl(c))
-	   printf("%d\n",c);
+	   printf("%d\r\n",c);
 	else
-	 printf("%d (%c)\n",c,c);
+	 printf("%d (%c)\r\n",c,c);
+	if(c== CTRL_KEY('q')) break;
 }
   return 0;
 }
