1fbefa420570cb0695af488950cebd45698f0e5a
Shubham <bluequbits@gmail.com>
2022-08-27 22:00:36 +0530

    Added global config and ~

--- kilo.c
+++ kilo.c
@@ -6,28 +6,40 @@
 #include <errno.h>
 
 //defines
-
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+/*data*/
+struct editorConfig{
 //original raw termios
 struct termios orig_termios;
 
+};
+
+struct editorConfig E;
+
+
+
+//function to show error when something goes wrong
 void die(const char*s){
+//clear screen
+write(STDOUT_FILENO,"\x1b[2J",4);
+//cursor positon to 1,1 row,col
+write(STDOUT_FILENO,"\x1b[H",3);
 	perror(s);
 	exit(1);
 }
 
 void disableRawMode(){
-  if (tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios)==-1)
+  if (tcsetattr(STDIN_FILENO,TCSAFLUSH,&E.orig_termios)==-1)
 	die("tcsetattr");
 }
 
 void enableRawMode(){
 
 
- if (tcgetattr(STDIN_FILENO,&orig_termios)==-1) die("tcgetattr");
+ if (tcgetattr(STDIN_FILENO,&E.orig_termios)==-1) die("tcgetattr");
 atexit(disableRawMode);
- struct termios raw = orig_termios;
+ struct termios raw = E.orig_termios;
  tcgetattr(STDIN_FILENO, &raw);
 //input flags to turn off to prevent weird behaviour by terminal
  raw.c_iflag &= ~(IXON|ICRNL|BRKINT|INPCK|ISTRIP);
@@ -56,12 +68,21 @@ return c;
 }
 
 //outputs
+void editorDrawRows(){
+ int y;
+	for(y=0;y<24;y++){
+	write(STDOUT_FILENO,"~\r\n",3);
+	}
+}
+
+
 void editorRefreshScreen(){
 
 write(STDOUT_FILENO,"\x1b[2J",4);
 //cursor positon
 write(STDOUT_FILENO,"\x1b[H",3);
-
+editorDrawRows();
+write(STDOUT_FILENO,"\x1b[H",3);
 }
 
 
@@ -69,6 +90,11 @@ void editorProcessKeypress(){
 	char c = editorReadKey();
 	switch(c){
 	case CTRL_KEY('q'):
+	//clear screen
+	write(STDOUT_FILENO,"\x1b[2J",4);
+	//cursor positon to 1,1 row,col
+	write(STDOUT_FILENO,"\x1b[H",3);
+
 	exit(0);
 	break;
 	}
