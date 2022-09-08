71afd3325b4959696661fe9cde99f0c596f7e2d6
Shubham <bluequbits@gmail.com>
2022-09-08 22:32:56 +0530

    added buffer for write statements

--- kilo.c
+++ kilo.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 
 // defines
 #define CTRL_KEY(k) ((k)&0x1f)
@@ -75,36 +76,40 @@ char editorReadKey()
 
 // terminal
 
-int getCursorPosition(int *rows, int *cols) {
-  char buf[32];
-  unsigned int i = 0;
-  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
-  while (i < sizeof(buf) - 1) {
-    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
-    if (buf[i] == 'R') break;
-    i++;
-  }
-  buf[i] = '\0';
-//   printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
-//   editorReadKey();
+int getCursorPosition(int *rows, int *cols)
+{
+	char buf[32];
+	unsigned int i = 0;
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+		return -1;
+	while (i < sizeof(buf) - 1)
+	{
+		if (read(STDIN_FILENO, &buf[i], 1) != 1)
+			break;
+		if (buf[i] == 'R')
+			break;
+		i++;
+	}
+	buf[i] = '\0';
+	//   printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+	//   editorReadKey();
 
-  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
-  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
-  return 0;
-  
+	if (buf[0] != '\x1b' || buf[1] != '[')
+		return -1;
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+		return -1;
+	return 0;
 }
-
 
 int getWindowSize(int *rows, int *cols)
 {
 	struct winsize ws;
 
-	if (1||ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
 	{
 		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
 			return -1;
 		return getCursorPosition(rows, cols);
-		
 	}
 	else
 	{
@@ -114,24 +119,59 @@ int getWindowSize(int *rows, int *cols)
 	}
 }
 
+// buffers
+/*
+this buffer is to write all the tilde lines to the screen at once
+
+*/
+struct abuf
+{
+	char *b;
+	int len;
+};
+
+#define ABUF_INIT {  NULL, 0 }
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+	char *new = realloc(ab->b, ab->len + len);
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+	free(ab->b);
+}
+
 // outputs
-void editorDrawRows()
+void editorDrawRows(struct abuf *ab)
 {
 	int y;
 	for (y = 0; y < E.screenrows; y++)
 	{
-		write(STDOUT_FILENO, "~\r\n", 3);
+		abAppend(&ab, "~", 1);
+
+		if (y < E.screenrows - 1)
+		{
+			abAppend(&ab, "\r\n", 2);
+		}
 	}
 }
 
 void editorRefreshScreen()
 {
+	struct abuf ab = ABUF_INIT;
 
-	write(STDOUT_FILENO, "\x1b[2J", 4);
+	abAppend(&ab, "\x1b[2J", 4);
 	// cursor positon
+	abAppend(&ab, "\x1b[H", 3);
+	editorDrawRows(&ab);
 	write(STDOUT_FILENO, "\x1b[H", 3);
-	editorDrawRows();
-	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	write(STDOUT_FILENO, ab.b, ab.len);
+	abFree(&ab);
 }
 
 void editorProcessKeypress()
@@ -154,8 +194,6 @@ void initEditor()
 {
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1)
 		die("getWindowSize");
-	
-	
 }
 
 int main()
